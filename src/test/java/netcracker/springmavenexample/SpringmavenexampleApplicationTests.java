661cd478fcdf9442b66d6534897c2c513761bc31
Ivan Shaldin <i-shaldin@mail.ru>
2020-10-18 19:56:07 +0300

    Initial commit

---
+++ src/test/java/netcracker/springmavenexample/SpringmavenexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package netcracker.springmavenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmavenexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
